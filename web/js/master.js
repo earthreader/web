9a29bb16e6dad4723417fd4db4273a895d34587a
kjwon15 <kjwonmail@gmail.com>
2013-09-28 03:01:36 +0900

    Don't support small screen message.

--- web/js/master.js
+++ web/js/master.js
@@ -1,3 +1,36 @@
+function resizer(event) {
+	var name = event.animationName;
+	var banner;
+
+	console.log(name);
+
+	if (name === "big") {
+		banner = document.querySelector('.banner');
+		banner.parentElement.removeChild(banner);
+	} else if (name === "small") {
+		banner = document.querySelector('.banner');
+
+		if (banner) {
+			return;
+		}
+
+		banner = document.createElement('div');
+		banner.style.position = "fixed";
+		banner.style.width = "100%";
+		banner.style.height = "100%";
+		banner.style.left = "0";
+		banner.style.top = "0";
+		banner.style.background = "white";
+		banner.style.textAlign = "center";
+		banner.style.zIndex = "10";
+		banner.className = "banner";
+
+		banner.textContent = "Earth reader doesn't support small screen.";
+
+		document.body.appendChild(banner);
+	}
+}
+
 function folderToggle(event) {
 	var target = event.target;
 	while (target.nodeName !== 'HEADER') {
@@ -17,6 +50,20 @@ function folderToggle(event) {
 function init() {
 	var navi = document.querySelector('[role=navigation]');
 	navi.addEventListener('click', folderToggle, false);
+
+    var animationEnd;
+    if (document.body.style.animation !== undefined) {
+        animationEnd = 'animationend';
+    } else if (document.body.style.MozAnimation !== undefined) {
+        animationEnd = 'animaionend';
+    } else if (document.body.style.webkitAnimation !== undefined) {
+        animationEnd = 'webkitAnimationEnd';
+    } else if (document.body.style.OAnimtion !== undefined) {
+        animationEnd = 'oAnimationEnd';
+    } else if (document.body.style.msAnimation !== undefined) {
+        animationEnd = 'MSAnimaionEnd';
+    }
+    document.body.addEventListener(animationEnd, resizer, false);
 }
 
 window.addEventListener('DOMContentLoaded', init, false);
