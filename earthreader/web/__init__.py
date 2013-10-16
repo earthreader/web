f34c1152d3dd9a7409731fb88dd249fe19d2e437
kjwon15 <kjwonmail@gmail.com>
2013-10-17 00:42:35 +0900

    I forgot restore modified __init__.py

--- earthreader/web/__init__.py
+++ earthreader/web/__init__.py
@@ -49,8 +49,6 @@ def get_feedlist():
 
 
 def get_hash(name):
-    print("Original: %s, hashed: %s"%(name,
-                                      hashlib.sha1(binary(name)).hexdigest()))
     return hashlib.sha1(binary(name)).hexdigest()
 
 
