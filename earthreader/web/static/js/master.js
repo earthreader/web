8997102badcfb212a7fabd10f12d726322d2bd09
kjwon15 <kjwonmail@gmail.com>
2013-11-16 01:49:23 +0900

    crawl feed manualy

--- earthreader/web/static/js/master.js
+++ earthreader/web/static/js/master.js
@@ -339,18 +339,27 @@ function appendEntry(entry) {
 	main.append(article);
 }
 
-function getEntries(feed_url) {
+function getEntries(feed_url, filter) {
 	var main = $('[role=main]');
-	$.get(feed_url, function(obj) {
-		main.html(null);
+	var requestUrl = feed_url + '?' + filter;
 
+	$.get(requestUrl, function(obj) {
 		var feed_title = obj.title;
 		var entries = obj.entries;
-
 		var header = $('<header>');
 		var h2 = $('<h2>');
-		header.append(h2);
+		var refresh = $('<a>');
+
+		main.html(null);
+
 		h2.text(feed_title);
+		header.append(h2);
+
+		refresh.addClass('refresh');
+		refresh.attr('role', 'button');
+		refresh.attr('href', feed_url);
+		refresh.text('refresh');
+		header.append(refresh);
 
 		main.append(header);
 
@@ -429,11 +438,7 @@ function reloadEntries() {
 	var filter = currentFilter.attr('data-filter');
 	var url = currentFeed.attr('data-entries') || currentFeed.parent().attr('data-entries') || URLS.entries;
 
-	//crawl newer feed
-	$.ajax(url, {
-		'type': 'PUT',
-	});
-	getEntries(url + filter);
+	getEntries(url, filter);
 }
 
 function clickFeed(event) {
@@ -531,6 +536,12 @@ function clickEntry(event) {
 	});
 }
 
+function refreshFeed(event) {
+	var target = $(event.target);
+	var url = target.attr('href');
+	$.ajax(url, {'type': 'PUT'});
+}
+
 function clickLink(event) {
 	var target = event.target;
 
@@ -625,6 +636,7 @@ $(function () {
 	var main = $('[role=main]');
 	main.on('click', '.entry-title', clickEntry);
 	main.on('click', '.nextPage', loadNextPage);
+	main.on('click', '.refresh', refreshFeed);
 
 	var side = $('[role=complementary]');
 	side.on('click', '[data-action]', clickComplementaryMenu);
