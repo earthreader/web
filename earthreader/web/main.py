617a13467cadd54e418d45816dee84cfd156fc11
Jihyeok Seo <me@limeburst.net>
2014-11-02 00:03:45 +0900

    Use libearth.subscribe.Subscription

--- earthreader/web/main.py
+++ earthreader/web/main.py
@@ -12,28 +12,31 @@ bp = Blueprint(__name__, 'main')
 
 @bp.route('/')
 def home():
-    feeds = get_feeds()
-    categories = get_categories()
-    return render_template('home.html', feeds=feeds, categories=categories)
+    with stage:
+        subscriptions = stage.subscriptions
+    return render_template('home.html', subscriptions=subscriptions)
 
 
-@bp.route('/entries/<feed_id>/')
+@bp.route('/<feed_id>/')
 def entries(feed_id):
-    feed = get_feed(feed_id)
+    with stage:
+        feed = stage.feeds[feed_id]
     return render_template('entries.html', feed=feed, feed_id=feed_id)
 
 
-@bp.route('/entry/<feed_id>/<path:entry_id>')
+@bp.route('/<feed_id>/<path:entry_id>')
 def entry(feed_id, entry_id):
-    feed = get_feed(feed_id)
+    with stage:
+        feed = stage.feeds[feed_id]
     entry = get_entry(feed, entry_id)
     return render_template('entry.html', feed=feed, feed_id=feed_id,
                            entry=entry)
 
 
-@bp.route('/category/<category_id>/')
-def category(category_id):
-    category = get_category(category_id)
+@bp.route('/category/<category_label>/')
+def category(category_label):
+    with stage:
+        category = stage.subscriptions.categories[category_label]
     return render_template('category.html', category=category)
 
 
@@ -42,23 +45,4 @@ def get_entry(feed, entry_id):
         for entry in feed.entries:
             if entry.id == entry_id:
                 return entry
-
-
-def get_feed(feed_id):
-    with stage:
-        return stage.feeds[feed_id]
-
-
-def get_feeds():
-    with stage:
-        return {feed_id: stage.feeds[feed_id] for feed_id in stage.feeds}
-
-
-def get_category(category_id):
-    with stage:
-        return stage.subscriptions.categories[category_id]
-
-
-def get_categories():
-    with stage:
-        return stage.subscriptions.categories
+    return None
